4b0899a9dfa4a716fe10c1a22f8a69679db2c20e
Kevin Kabore <kevin.s.kabore@gmail.com>
2019-06-17 01:55:30 -0700

    adds styling for modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -11,13 +11,36 @@ export default function Modal() {
 
   return (
     <div className="Modal">
-      <div className="modal-header"> New Notifications </div>{' '}
+      <div className="modal-header"> New Notifications </div> <hr />{' '}
       {cards.map((c, i) => {
         return (
-          <div>
-            <ReactMarkdown key={c.id + `-title`} source={c.title} />{' '}
-            <ReactMarkdown key={c.id + `-date}`} source={c.date} />{' '}
-            <ReactMarkdown key={c.id + `-desc`} source={c.desc} />{' '}
+          <div className="card">
+            <div className="card-header">
+              <ReactMarkdown
+                style={{
+                  margin: 0
+                }}
+                className="card-title"
+                key={c.id + `-title`}
+                source={c.title}
+              />{' '}
+              <ReactMarkdown
+                style={{
+                  margin: 0
+                }}
+                className="card-date"
+                key={c.id + `-date}`}
+                source={c.date}
+              />{' '}
+            </div>{' '}
+            <ReactMarkdown
+              style={{
+                margin: 0
+              }}
+              className="card-description"
+              key={c.id + `-desc`}
+              source={c.desc}
+            />{' '}
           </div>
         );
       })}{' '}
